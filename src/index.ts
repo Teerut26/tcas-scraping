253fbc497091e634fc29798db1a1e62f4c60d015
Teerut26 <teerutsr@gmail.com>
2022-05-02 17:22:03 +0700

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
-const geeting = (name: string) => {
-  console.log(`Hello ${name} from TypeScript`);
+import axios from "axios";
+import { promises } from "fs";
+
+interface Root {
+    _id: string;
+    university_id: string;
+    university_type: string;
+    university_name: string;
+    university_name_en: string;
+    file_path_1?: string;
+    is_accepted_round1: boolean;
+    is_accepted_round2: boolean;
+    is_accepted_round3: boolean;
+    is_accepted_round4: boolean;
+    file_path_2?: string;
+    file_path_3?: string;
+    file_path_4?: string;
+    file_path_handicap?: string;
+}
+
+const getData = async () => {
+    let { data } = await axios.get<Root[]>(
+        "https://tcas65.s3.ap-southeast-1.amazonaws.com/mytcas/universities.json"
+    );
+    let newData = data.map((item) => ({
+        university_id: item.university_id,
+        university_name: item.university_name,
+        university_name_en: item.university_name_en,
+    }));
+
+    newData.map((item) => {
+        getImage(item.university_id);
+    });
+
+    // let resImage = await axios.get();
+    // console.log(JSON.stringify(newData));
+    // promises.writeFile("data.json",JSON.stringify(newData))
 };
 
-geeting("Teerut");
+const getImage = async (id: string) => {
+    let { data } = await axios.get(
+        `https://www.mytcas.com/assets/img/logo/${id}.png`,
+        {
+            responseType: "arraybuffer",
+        }
+    );
+    await promises.writeFile(`./image/${id}.png`, data);
+};
 
-//ref https://dev.to/somprasongd/typescript-node-js-jb7
+getData()
