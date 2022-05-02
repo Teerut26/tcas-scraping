0699d1d22124ac4e2305b40fbcb54f6260381397
Teerut Srithongdee <46746401+Teerut26@users.noreply.github.com>
2022-05-02 16:54:33 +0700

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const geeting = (name) => {
+    console.log(`Hello ${name} from TypeScript`);
+};
+geeting("Teerut");
+//ref https://dev.to/somprasongd/typescript-node-js-jb7
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const geeting = (name: string) => {
+  console.log(`Hello ${name} from TypeScript`);
+};
+
+geeting("Teerut");
+
+//ref https://dev.to/somprasongd/typescript-node-js-jb7
